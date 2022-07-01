2e11cae3b1a219e94c3de0cfe8976bf8dbf1ccdd
AbduljabbarSal <asalk025@uottawa.ca>
2022-06-30 23:21:56 -0400

    Updates

---
+++ precache-manifest.5f6d5988c9afec564fdc2bc88551d585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7ed8782bef4eab5a8580a9b746911c",
+    "url": "/netlifyReactTodoList/index.html"
+  },
+  {
+    "revision": "490672ff9d14f8af5966",
+    "url": "/netlifyReactTodoList/static/css/main.3877c57a.chunk.css"
+  },
+  {
+    "revision": "d587e1f22dd003171289",
+    "url": "/netlifyReactTodoList/static/js/2.83f902b7.chunk.js"
+  },
+  {
+    "revision": "490672ff9d14f8af5966",
+    "url": "/netlifyReactTodoList/static/js/main.d5f74a7a.chunk.js"
+  },
+  {
+    "revision": "4681f6451f13c12cb3c7",
+    "url": "/netlifyReactTodoList/static/js/runtime~main.33d2f582.js"
+  }
+]);
